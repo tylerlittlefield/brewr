07305319964abb75ae0e38b86416513f41ce810a
tylurp <tylurp1@gmail.com>
2018-11-04 17:45:12 -0800

    Added analytics dataset for testing purposes mostly. Also for users that want a list of all analytics endpoints.

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Endpoints ffrom Homebrews analytics API.
+#'
+#' A dataset containing a list of 19 analytics endpoints. This doesn't include
+#' formula endpoints e.g. /formula/sqlite.
+#'
+#' @format A data frame with 19 rows and 1 variable:
+#' \describe{
+#'   \item{analytics}{a list of all analytics endpoints}
+#' }
+#' @source \url{https://formulae.brew.sh/docs/api/}
+"analytics"
--- tests/testthat/test-brewr.R
+++ tests/testthat/test-brewr.R
@@ -1,6 +1,6 @@
 context("test-brewr")
 
 test_that("brewr works", {
-  # Test that a request is valid json
-  expect_true(jsonlite::validate(brewr("/formula/sqlite")))
+  # Test that a request is valid json, samples 21 analytics end points
+  expect_true(jsonlite::validate(brewr(sample(analytics[["analytics"]], 1))))
 })
